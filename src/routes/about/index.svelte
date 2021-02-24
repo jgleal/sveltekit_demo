9f82f587f115b280a6deace493e58d6bf668347f
Javier G. Leal <jgleal@gmail.com>
2021-02-24 16:22:28 +0100

    feat: add counter in about page

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,7 +1,29 @@
 <script>
 	import { count } from '$stores/count'
 </script>
-<h1>Counter {$count}</h1>
+<style>
+    #counter {
+        margin-top: 1em;
+    }
+    input[type="number"] {
+        -moz-appearance: textfield;
+        width: 2em;
+        color: #f5c262;
+        text-align: center;
+    }
+    input[type="number"]::-webkit-inner-spin-button, 
+    input[type="number"]::-webkit-outer-spin-button { 
+        -webkit-appearance: none; 
+        margin: 0; 
+    }
+    label, input {
+        font-size: 3rem;
+    }
+</style>
+<div id="counter">
+    <label for="counter">Counter:</label>
+    <input id="counter" type="number" bind:value={$count}>
+</div>
 <p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
 
 
